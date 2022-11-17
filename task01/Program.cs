bf82cc49a1f237d0387bb88bebfc9115bbb155a3
oldmash <kiselev.nik.v@gmail.com>
2022-11-17 15:00:42 +0300

    initial commit

---
+++ task01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+using Animals;
+namespace task01;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Animal));
+        var animalToSererialize = new Animal("RU", false, "Oleg", "Copibara");
+        using (FileStream fs = new FileStream("../../../animal.xml", FileMode.Truncate))
+        {
+            xmlSerializer.Serialize(fs, animalToSererialize);
+            Console.WriteLine("Object has been serialized");
+            
+        }
+
+        using (FileStream fs = new FileStream("../../../animal.xml", FileMode.OpenOrCreate))
+        {
+            var deserializeAnimal = xmlSerializer.Deserialize(fs) as Animal;
+            Console.WriteLine($"deserialized object: " +
+                              $"\ntype: {deserializeAnimal}:" +
+                              $"\n\tcountry: {deserializeAnimal.Country}" +
+                              $"\n\tname: {deserializeAnimal.Name}" +
+                              $"\n\thide from animals: {deserializeAnimal.HideFromOtherAnimals}" +
+                              $"\n\twhat animal: {deserializeAnimal.WhatAnimal}");
+            
+        }
+    }
+}
---
+++ task02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace task02;
+
+using System.IO;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("");
+        const string fileToSearch = "YEEEEES.txt";
+        var path = Directory.GetFiles("/Users/oldmash/RiderProjects", fileToSearch, SearchOption.AllDirectories);
+        Console.WriteLine($"Путь к файлу{path[0]}");
+        using (FileStream fs = new FileStream(path[0], FileMode.Open))
+        {
+            byte[] b = new byte[fs.Length];
+            UTF8Encoding temp = new UTF8Encoding(true);
+            Console.WriteLine("Содержимое искомого файла:\n<");
+            while (fs.Read(b, 0, b.Length) > 0)
+            {
+                Console.WriteLine(temp.GetString(b));
+            }
+
+            Console.WriteLine(">");
+        }
+
+        var zipPath = "/Users/oldmash/RiderProjects/labsC#/C-_lab08/task01/YEEEEES.zip";
+        using (FileStream fs = new FileStream(path[0], FileMode.Open))
+        {
+            using FileStream targetStream = File.Create(zipPath);
+            using GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress);
+            fs.CopyTo(compressionStream);
+            Console.WriteLine($"Файл сжат в {zipPath}!");
+        }
+    }
+}
